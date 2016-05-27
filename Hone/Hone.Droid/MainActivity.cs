37b14aba8baef86011033ab32e8bcdd3df5baa2c
Brian Souza <brian.diego.souza@gmail.com>
2016-05-26 23:34:26 -0300

    Criar temas

--- Hone/Hone.Droid/MainActivity.cs
+++ Hone/Hone.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Hone.Droid
 {
-    [Activity(Label = "Hone", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Theme = "@android:style/Theme.Material.Light", Label = "Hone", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
