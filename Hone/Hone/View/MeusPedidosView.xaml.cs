d42bea884d451a58eece67c142fbee4a3110687d
Brian Souza <brian.diego.souza@gmail.com>
2016-06-08 00:13:38 -0300

    lista de pedidos

---
+++ Hone/Hone/View/MeusPedidosView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Hone.View
+{
+    public partial class MeusPedidosView : ContentPage
+    {
+        public MeusPedidosView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Hone/Hone/ViewModel/MeusPedidosViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hone.Entidades;
+
+namespace Hone.ViewModel
+{
+    public class MeusPedidosViewModel : BaseViewModel
+    {
+        public MeusPedidosViewModel()
+        {
+
+        }
+
+        private ObservableCollection<Pedido> lstPedidos;
+        private DateTime dataDe;
+        private DateTime dataAte;
+
+        public DateTime DataAte
+        {
+            get
+            {
+                return dataAte;
+            }
+
+            set
+            {
+                dataAte = value;
+                this.Notify("DataAte");
+            }
+        }
+
+        public DateTime DataDe
+        {
+            get
+            {
+                return dataDe;
+            }
+
+            set
+            {
+                dataDe = value;
+                this.Notify("dataDe");
+            }
+        }
+
+        public ObservableCollection<Pedido> LstPedidos
+        {
+            get
+            {
+                return lstPedidos;
+            }
+
+            set
+            {
+                lstPedidos = value;
+                this.Notify("LstPedidos");
+            }
+        }
+    }
+}
