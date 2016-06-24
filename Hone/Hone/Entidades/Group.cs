57deb0c50321cf190a6108306b811a1b98d7e234
Brian Souza <brian.diego.souza@gmail.com>
2016-06-23 23:19:38 -0300

    criando form de pesquisa de parceiro

---
+++ Hone/Hone/Entidades/Group.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hone.Entidades
+{
+    public class Group<Tkey,TItem> : ObservableCollection<TItem>
+    {
+        public Tkey Key { get; private set; }
+
+        public Group(Tkey key,IEnumerable<TItem> items)
+        {
+            this.Key = key;
+            foreach(var item in items)
+            {
+                this.Items.Add(item);
+            }
+        }
+    }
+}
---
+++ Hone/Hone/ViewModel/PNViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Hone.Entidades;
+using Xamarin.Forms;
+
+namespace Hone.ViewModel
+{
+    public class PNViewModel : BaseViewModel
+    {
+        public PNViewModel()
+        {
+            Filtrar = new Command(FiltrarPN);
+        }
+
+        private ObservableCollection<Parceiro> listaParceiros;
+
+        public ICommand Filtrar
+        {
+            get;
+            set;
+        }
+
+        public ObservableCollection<Parceiro> ListaParceiros
+        {
+            get
+            {
+                return listaParceiros;
+            }
+
+            set
+            {
+                listaParceiros = value;
+                this.Notify("ListaParceiros");
+            }
+        }
+
+        private IEnumerable<Group<char,Parceiro>> FiltrarPN()
+        {
+            return from pn in ListaParceiros
+                   orderby pn.CardName
+                   group pn by pn.CardName[0] into grupos
+                   select new Group<char, Parceiro> ( grupos.Key, grupos );
+        }
+    }
+}
