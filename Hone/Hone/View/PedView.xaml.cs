a66874f0d4d0699332882299245c24a210d34ea8
Brian Souza <brian.diego.souza@gmail.com>
2016-08-01 21:29:28 -0300

    Visualizar pedido, funcionando.

--- Hone/Hone/View/PedView.xaml.cs
+++ Hone/Hone/View/PedView.xaml.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Hone.ViewModel;
 using Xamarin.Forms;
 
 namespace Hone.View
 {
     public partial class PedView : ContentPage
     {
+        PedViewModel pedVM = null;
         public PedView()
         {
             InitializeComponent();
+            pedVM = new PedViewModel();
+            this.BindingContext = pedVM;
         }
     }
 }
